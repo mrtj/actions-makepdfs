22135f3b19a7c920b290fd7b5f1e2fab939bb1a5
Janos Tolgyesi <janos.tolgyesi@gmail.com>
2020-03-26 14:59:26 +0100

    Disable header & footer

--- makepdfs.js
+++ makepdfs.js
@@ -11,12 +11,6 @@ var fs = require( 'fs' );
 var mddir = '/github/workspace/' + markdown_dir;
 var dir = '/github/workspace/' + output_dir + '/';
 
-fs.readdir('/node_modules/puppeteer/.local-chromium', (err, files) => {
-  files.forEach(file => {
-    console.log(file);
-  });
-});
-
 /* 
  * Show an error message 
  */
@@ -70,10 +64,10 @@ function makePdf(data,file) {
 				path: dir + file + '.pdf',
 				format: 'A4',
 				scale: .9,
-				displayHeaderFooter: true,
-				margin: {top: 100, bottom:100, right: '50', left: '50'},
-				footerTemplate: '<div class="pageNumber" style="font-size:8px;width:100%;text-align:center;"></div>',
-				headerTemplate: '<div class="date" style="font-size:8px;width:100%;text-align:center;"></div>'
+				displayHeaderFooter: false,
+				margin: {top: 100, bottom:100, right: '50', left: '50'}
+// 				footerTemplate: '<div class="pageNumber" style="font-size:8px;width:100%;text-align:center;"></div>',
+// 				headerTemplate: '<div class="date" style="font-size:8px;width:100%;text-align:center;"></div>'
 			});
 		  
 			await browser.close();
@@ -107,31 +101,31 @@ fs.readdir (mddir,function(err, files) {
    console.log('files found: ' + files);
    for (let file of files) {
       if (path.extname(file) == '.md') {				 
-				var text = fs.readFileSync(mddir + '/' + file).toString('utf-8');
-				var md = require('markdown-it')({
-					html: true,
-					breaks: true,
-					highlight: function (str, lang) {
-						if (lang && hljs.getLanguage(lang)) {
-						try {
-							str = hljs.highlight(lang, str, true).value;
-						} catch (error) {
-							str = md.utils.escapeHtml(str);
-				
-							showErrorMessage('markdown-it:highlight', error);
-						}
-						} else {
-						str = md.utils.escapeHtml(str);
-						}
-						return '<pre class="hljs"><code><div>' + str + '</div></code></pre>';
-					}
-				});
-				var options = {
-						slugify: Slug
-				}
-				md.use(require('markdown-it-named-headers'), options);
-				var body = md.render(text);
-				makePdf('data:text/html;,' + encodeURIComponent(makeHtml(body)),file);
+	var text = fs.readFileSync(mddir + '/' + file).toString('utf-8');
+	var md = require('markdown-it')({
+		html: true,
+		breaks: true,
+		highlight: function (str, lang) {
+			if (lang && hljs.getLanguage(lang)) {
+			try {
+				str = hljs.highlight(lang, str, true).value;
+			} catch (error) {
+				str = md.utils.escapeHtml(str);
+
+				showErrorMessage('markdown-it:highlight', error);
+			}
+			} else {
+			str = md.utils.escapeHtml(str);
+			}
+			return '<pre class="hljs"><code><div>' + str + '</div></code></pre>';
+		}
+	});
+	var options = {
+		slugify: Slug
+	}
+	md.use(require('markdown-it-named-headers'), options);
+	var body = md.render(text);
+	makePdf('data:text/html;,' + encodeURIComponent(makeHtml(body)),file);
       }
    }
 });
